1dfbd2ae8d0412c2dfc9730524ed5f960d27e4af
sunil.kumar <sunil.kumar07193102@gmail.com>
2025-09-21 21:18:22 +0530

    message

--- Variables3.sh
+++ Variables3.sh
@@ -1,6 +1,6 @@
 #! bin/bash
-start_time=$(data +%s)
+start_time=$(date +%s)
 sleep 10
-end_time=$(data +%s)
+end_time=$(date +%s)
 total_time=end_time-start_time
 echo "total time utilized to run the script: $total_time"
