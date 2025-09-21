e8235c3a68cb5e2b0cc6adbe99a7acde9a19de3d
sunil.kumar <sunil.kumar07193102@gmail.com>
2025-09-21 21:46:10 +0530

    message

--- Variables4.sh
+++ Variables4.sh
@@ -2,7 +2,7 @@
 echo "all variables passed to the script: $*"
 echo "what is present working directory: $PWD"
 echo "who is running this script: $USER"
-echo "what is time and dat this script has ran: $time"
+echo "what is time and date this script has ran: $date"
 echo "what is the PID of this script: $$"
 echo "what is the script file name: $0"
 echo "what is the Home directory of the user:$HOME"
