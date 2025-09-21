a4ae3f25bd6e8c98de0e7ab1380727480c951311
sunil.kumar <sunil.kumar07193102@gmail.com>
2025-09-21 21:23:58 +0530

    message

--- Variables3.sh
+++ Variables3.sh
@@ -2,5 +2,5 @@
 start_time=$(date +%s)
 sleep 10
 end_time=$(date +%s)
-total_time=(($end_time-$start_time))
+total_time=(($end_time - $start_time))
 echo "total time utilized to run the script: $total_time"
