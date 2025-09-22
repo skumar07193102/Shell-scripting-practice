5745d84e38678fdea33675f23418f5e98080fed1
sunil.kumar <sunil.kumar07193102@gmail.com>
2025-09-22 23:42:30 +0530

    some

--- root-previliges-Exit-code.sh
+++ root-previliges-Exit-code.sh
@@ -4,7 +4,7 @@ if [ $user -ne 0 ]; then
     echo " Command has to run with root previliges"
     exit 1
 fi
-dnf install mysql1111 -y
+dnf install mysql -y
 if [ $? -ne 0 ]; then
     echo "installation failed"
     exit 1
