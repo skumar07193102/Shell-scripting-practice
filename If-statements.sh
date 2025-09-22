1dce5a2f1a54f0c1f9dcc38bd64bd49700555b3d
sunil.kumar <sunil.kumar07193102@gmail.com>
2025-09-22 22:47:03 +0530

    some

--- If-statements.sh
+++ If-statements.sh
@@ -2,4 +2,8 @@
 Number=$1
 if [ $Number -lt 10 ]; then
     echo " this $Number is less than 10"
+elif [ $Number -eq 10]; then
+    echo " this $Number is equal to 10"
+else
+    echo " this $number is greater than 10"
 fi
