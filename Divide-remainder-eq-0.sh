89016dfbe873ed4292d5bea34aa467301f9288fd
sunil.kumar <sunil.kumar07193102@gmail.com>
2025-09-22 23:01:05 +0530

    some

--- Divide-remainder-eq-0.sh
+++ Divide-remainder-eq-0.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 echo " Enter the number"
 read v1
-if [ $((v1%2 -eq 0)) ]; then
+if [ $(($v1 % 2)) -eq 0 ]; then
     echo "this $v1 a prime number"
 else
     echo "this $v1 is odd number"
