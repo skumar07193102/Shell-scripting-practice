973d491d306f4944afdeb1df2375a9cc4aa5fd1c
sunil.kumar <sunil.kumar07193102@gmail.com>
2025-09-27 21:59:56 +0530

    s

--- validate-function.sh
+++ validate-function.sh
@@ -7,10 +7,10 @@ else
     echo -e "\e[33m successfully ran with root previlieges"
 fi
 VALIDATE(){
-    if [ $1 -eq 0 ]; then
+if [ $1 -eq 0 ]; then
     echo -e "\e[32m $2 is already installed"
     exit 1
-    else
+else
     dnf install $2 -y
     echo -e "\e[34m successfully installed $2"
 fi
