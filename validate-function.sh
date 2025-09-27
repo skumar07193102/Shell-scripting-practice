196fd7373493af88b460a882aadea2f3f8baa8a3
sunil.kumar <sunil.kumar07193102@gmail.com>
2025-09-27 22:02:00 +0530

    s

--- validate-function.sh
+++ validate-function.sh
@@ -4,12 +4,11 @@ if [ $user -ne 0 ]; then
     echo " plese run with root previliges"
     exit 1
 else
-    echo -e "\e[33m successfully ran with root previlieges"
+    echo "successfully ran with root previlieges"
 fi
 VALIDATE(){
 if [ $1 -eq 0 ]; then
     echo "$2 is already installed"
-    exit 1
 else
     dnf install $2 -y
     echo  "successfully installed $2"
