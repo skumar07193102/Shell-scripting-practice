3013cbaa9430447b6f6cdf6eff41af1daf6ef915
sunil.kumar <sunil.kumar07193102@gmail.com>
2025-09-27 20:50:30 +0530

    s

--- Command-installation.sh
+++ Command-installation.sh
@@ -2,14 +2,21 @@
 user=$(id -u)
 if [ $user -ne 0 ]; then
     echo " plese run with root previliges"
+    exit 1
 else
     echo "successfully ran with root previlieges"
-    exit 1
 fi
-
 dnf install nodejs:20 -y
 if [ $? -eq 0 ]; then
     echo " Nodejs Installation is sucessfull"
+    exit 1
 else
     echo " Installation failed"
+fi
+dnf install mysqlll -y
+if [ $? -eq 0 ]; then
+    echo " mysql Installation is sucessfull"
+    exit 1
+else
+    echo " mysql Installation failed"
 fi
