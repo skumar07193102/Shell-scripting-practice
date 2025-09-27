02487fbc83f7265306c43349f88e3ea3bd821b83
sunil.kumar <sunil.kumar07193102@gmail.com>
2025-09-27 23:09:24 +0530

    s

---
+++ shellscripting-logs.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+user=$(id -u)
+Logfolder=/var/log/shell-script
+scriptname=$( echo $0 | cut -d "." -f1)
+logfile=$Logfolder/$scriptname.log
+mkdir -p $Logfolder
+echo "script executed time is $(date)"
+if [ $user -ne 0 ]; then
+    echo " plese run with root previliges"
+    exit 1
+else
+    echo "successfully ran with root previlieges"
+fi
+VALIDATE(){
+if [ $1 -eq 0 ]; then
+    echo "$2 is already installed"
+else
+    dnf install $2 -y
+    echo  "successfully installed $2"
+fi
+}
+dnf list installed mysql-server &>>$logfile
+VALIDATE $? mysql-server
+
+dnf list installed telnet &>>$logfile
+VALIDATE $? telnet
