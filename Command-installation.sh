052c1c7e1c95170bba2f9c74d2a5f4cce1115711
sunil.kumar <sunil.kumar07193102@gmail.com>
2025-09-27 19:22:46 +0530

    s

--- Command-installation.sh
+++ Command-installation.sh
@@ -3,5 +3,5 @@ user=$(id -u)
 if [ $user -ne 0 ]; then
     echo " plese run with root previliges"
 else
-    echo" successfully ran with root previlieges"
+    echo "successfully ran with root previlieges"
 fi
