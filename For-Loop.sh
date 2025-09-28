5205caa7812d4a5037afc19057afa8351e3c30a2
sunil.kumar <sunil.kumar07193102@gmail.com>
2025-09-28 12:55:48 +0530

    s

---
+++ For-Loop.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+user=$(id -u)
+Logfolder=/var/log/shell-script
+scriptname=$( echo $0 | cut -d "." -f1)
+logfile=$Logfolder/$scriptname.log
+mkdir -p $Logfolder
+echo "script executed time is $(date)"
+if [ $user -ne 0 ]; then
+    echo " plese run with root previliges"
+    exit 1
+else
+    echo "successfully ran with root previlieges"
+fi
+VALIDATE(){
+if [ $1 -eq 0 ]; then
+    echo "$2 installation is $G Success $N"
+else
+    echo  " $2 installtion is $R Failed $N"
+fi
+}
+for packagename in {$1,$2}
+do
+dnf list installed $packagename 
+if [ $? -ne 0 ]; then
+    dnf install $packagename -y
+    VALIDATE $? $packagename
+else 
+    echo " $packagename is already installed $Y SKIPPING $N"
+fi
+done
